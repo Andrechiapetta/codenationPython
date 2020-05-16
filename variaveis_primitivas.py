c1a11c51650900199be7b7dec6389be38b2fbbde
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-05-16 12:15:32 -0300

    aula 001 codenation

---
+++ variaveis_primitivas.py
@@ -0,0 +1,26 @@
+nome = "André Chiapetta"
+idade = 32
+altura = 1.79
+tutor = False
+trabalhando = None
+
+print (nome)
+print (idade)
+print (altura)
+print (tutor)
+print (trabalhando)
+
+nova_idade = idade + 30
+
+nome_cidade = nome + "São Paulo"
+
+print (nova_idade)
+print (nome_cidade)
+
+altura_sem_tenis = altura - 0.05
+
+print (altura_sem_tenis)
+
+resultado = altura + idade
+
+print (resultado)
