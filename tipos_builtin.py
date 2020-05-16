598b073bd12122998b22a7c4d60bd19e700a7ce3
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-05-16 12:36:42 -0300

    aula 002

---
+++ tipos_builtin.py
@@ -0,0 +1,35 @@
+lista_convidados = ["Andre", "Ana", "cassio", "Ph"]
+
+print (lista_convidados)
+
+lista_convidados.append ("Luara")
+
+print (lista_convidados)
+
+lista_convidados.remove ("Ph")
+
+print (lista_convidados)
+
+print (lista_convidados[0])
+print (lista_convidados[-1])
+print (lista_convidados[1])
+
+tupla1 = (1,2,3)
+tupla2 = (3,4,5)
+print (tupla1)
+print (tupla2)
+
+tupla3 = tupla1 + tupla2
+print (tupla3)
+
+dados_pessoais = {"nome": "Batman", "cidade": "Gothan"}
+
+print(dados_pessoais)
+
+dados_pessoais["veiculo"] = "Batmovel"
+
+print (dados_pessoais)
+
+del dados_pessoais["cidade"]
+
+print (dados_pessoais)
