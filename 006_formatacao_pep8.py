bacf032bfb2e9993ea7d9c941444223fe73bde37
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-05-17 12:59:43 -0300

    aula 006

---
+++ 006_formatacao_pep8.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+a = []
+
+b = ("cddff fdgg gdgdgd gdgddgdg gdgdhfhf hfhfhfhf fhfhfhfh"
+     "hfhfhfh rrtrtrh hrhrh hhbgbf wedeef gergrg")
+print(b)
+
+idade = 20
+
+if idade > 18:
+    print("maior de idade")
+
+print(sys.argv)
+print(os.times())
+
+nome = "André"
+
+print(f"Nome: {nome}")
