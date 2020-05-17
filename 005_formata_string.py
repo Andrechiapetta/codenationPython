301fe38359ea1eeda2103571bb899f1d6dd27b5a
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-05-17 12:39:39 -0300

    aula 005

---
+++ 005_formata_string.py
@@ -0,0 +1,21 @@
+idade = 32
+
+print("Minha idade é: " + str(idade))
+
+print("Minha idade é: {}".format(idade))
+
+print( f"Minha idade é: {idade}")
+
+nome = "André luiz da Silva Chiapetta"
+
+print(f"Meu nome é {nome:.10} e eu tenho {idade:03} anos")
+
+dinheiro = 2.5
+
+print(f"Eu tenho R${dinheiro:.2f}")
+
+listaItens = ["Garfo", "Faca", "Copo", "Prato"]
+
+print(f"Eu almoço com {listaItens[0]} e {listaItens[1]} no {listaItens[-1]}")
+
+print(f"Eu terei {idade + 30} anos daqui a 30 anos")
